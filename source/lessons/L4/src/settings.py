cc1327e6ed74881b93219e7c1f0f478f4779bef3
Vallejo Bryan <bryan.vallejo@aalto.fi>
2024-05-22 14:37:34 +0300

    hpc draft

--- source/lessons/L4/src/settings.py
+++ source/lessons/L4/src/settings.py
@@ -1,6 +1,6 @@
 layer_specs = {
     2005: {
-        'path_tiles': '/projappl/project_2009245/GeoHPC/source/lessons/L4/2005_tiles',
+        'path_tiles': '/projappl/project_200xxx/GeoHPC/source/lessons/L4/2005_tiles',
         'join_attribute_name': 'KOHDEOSO',
         'layers': {
             0: {
